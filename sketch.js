748bd9b43f18c99a41f260456f7be24ff49f6621
Shaked Lokits <slokits@lightricks.com>
2018-10-16 19:33:07 +0300

    Initial commit.

---
+++ sketch.js
@@ -0,0 +1,18 @@
+/**
+ * @Author: Shaked Lokits <slokits>
+ * @Date:   2018-10-14T14:04:39+03:00
+ * @Email:  shaked.lokits@gmail.com
+ * @Filename: sketch.js
+ * @Last modified by:   slokits
+ * @Last modified time: 2018-10-16T19:29:42+03:00
+ */
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {}
+
+function mouseDragged() {}
